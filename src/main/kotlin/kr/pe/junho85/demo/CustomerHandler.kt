b60f31e81881a9d50d6cdfb285566609e26f7e20
june.kim <junho85@gmail.com>
2020-09-30 22:59:05 +0900

    ~p204 CustomerHandler

---
+++ src/main/kotlin/kr/pe/junho85/demo/CustomerHandler.kt
@@ -0,0 +1,17 @@
+package kr.pe.junho85.demo
+
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+
+@Component
+class CustomerHandler {
+//    fun get(serverRequest: ServerRequest): Mono<ServerResponse> {
+//        return ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
+//    }
+    fun get(serverRequest: ServerRequest) =
+        ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
+}
--- src/main/kotlin/kr/pe/junho85/demo/CustomerRouter.kt
+++ src/main/kotlin/kr/pe/junho85/demo/CustomerRouter.kt
@@ -1,22 +1,26 @@
 package kr.pe.junho85.demo
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.router
-import reactor.kotlin.core.publisher.toMono
 
 @Component
 class CustomerRouter {
+    @Autowired
+    lateinit var customerHandler: CustomerHandler
+
     @Bean
     fun customerRoutes(): RouterFunction<*> = router {
         "/functional".nest {
             "/customer".nest {
                 GET("/") {
 //                    ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
-                    it: ServerRequest ->
-                        ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
+//                    it: ServerRequest ->
+//                        ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
+                    it: ServerRequest -> customerHandler.get(it)
                 }
             }
         }
