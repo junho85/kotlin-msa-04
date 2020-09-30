169329db2bd006cd0ac21789870bb543acc7ec63
june.kim <junho85@gmail.com>
2020-09-30 22:53:12 +0900

    ~p203 람다 자세히 들여다보기

--- src/main/kotlin/kr/pe/junho85/demo/CustomerRouter.kt
+++ src/main/kotlin/kr/pe/junho85/demo/CustomerRouter.kt
@@ -3,6 +3,7 @@ package kr.pe.junho85.demo
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.router
 import reactor.kotlin.core.publisher.toMono
 
@@ -13,7 +14,9 @@ class CustomerRouter {
         "/functional".nest {
             "/customer".nest {
                 GET("/") {
-                    ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
+//                    ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
+                    it: ServerRequest ->
+                        ok().body(Customer(1, "functional web").toMono(), Customer::class.java)
                 }
             }
         }
