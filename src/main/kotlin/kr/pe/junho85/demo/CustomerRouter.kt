7e0cdd7f4ab57ebbbfed111be18d80adecd9e7f2
june.kim <junho85@gmail.com>
2020-09-30 22:43:17 +0900

    ~p200

---
+++ src/main/kotlin/kr/pe/junho85/demo/CustomerRouter.kt
@@ -0,0 +1,22 @@
+package kr.pe.junho85.demo
+
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
+import reactor.kotlin.core.publisher.toMono
+
+@Component
+class CustomerRouter {
+    @Bean
+    fun customerRoutes(): RouterFunction<*> = router {
+        "/functional".nest {
+            "/customer".nest {
+                GET("/") {
+                    ServerResponse.ok().body("hello world".toMono(), String::class.java)
+                }
+            }
+        }
+    }
+}
